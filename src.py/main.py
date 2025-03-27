6553ff63de3988f6c0f9904aa21b291c4d4eaa67
Ramirezsara09 <alvarezz.sara@uces.edu.co>
2025-03-27 11:06:25 -0500

    Creación de la función principal y funcionamiento cíclico del código

--- src.py/main.py
+++ src.py/main.py
@@ -13,7 +13,33 @@ def validar_movimiento():
     pass
 def analizar_ritmo():
     pass
+def iniciar_registro():
+    pass
 def registro_lectura():
     pass
-def main():
-    pass
+def main():  
+    paciente = inicializar_paciente() 
+    inicio = time.time()
+    
+    print('Sistems de monitoreo para UCI/UCE')
+    print('Iniciando monitoreo durante 60 segundos')
+   
+    try:
+        iniciar_registro()
+        while (time.time() - inicio) < 60: 
+            movimiento, ritmo = simular_lecturas()
+            paciente['movimiento_actual'] = movimiento 
+            paciente['ritmo_actual'] = ritmo 
+            
+            analizar_ritmo(paciente)
+            registro_lectura(paciente)
+            time.sleep(1) 
+        
+        print('Monitoreo finalizado')
+        
+    except KeyboardInterrupt:
+        print('Interrupcion de emergencia')
+        
+
+if __name__ == 'main': #con esto hacemos que el cdigo inicie por la funcion main()
+    main
