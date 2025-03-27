5e5e676e4767bd365ab11926befd3a8d591b3ef8
Ramirezsara09 <alvarezz.sara@uces.edu.co>
2025-03-27 08:41:52 -0500

    funciones

--- src.py/main.py
+++ src.py/main.py
@@ -5,3 +5,15 @@ UMBRAL_MOVIMIENTO = 0.5
 RITMO_MINIMO = 60
 RITMO_MAXIMO = 100
 
+def inicializar_paciente():
+    pass
+def simular_lecturas():
+    pass
+def validar_movimiento():
+    pass
+def analizar_ritmo():
+    pass
+def registro_lectura():
+    pass
+def main():
+    pass
