32fa7052fdf5cc7702760c95181045247fff0c70
manuela-zapata <zapatac.manuela@uces.edu.co>
2025-03-31 12:25:57 -0500

    Cambios en feature Manuela

--- src.py/main.py
+++ src.py/main.py
@@ -5,7 +5,7 @@ import random
 UMBRAL_MOVIMIENTO = 0.5  #valor límite 0.5, toma valores >0.5
 FRECUENCIA_CARDIACA_MINIMA = 60  #valores limites (max, min) que puede tomar       
 FRECUENCIA_CARDIACA_MAXIMA = 100  
-contador = 0
+
 
 def inicializar_paciente():
     return {
@@ -24,12 +24,33 @@ def simular_lecturas():
         print(f"Error en simulación: {str(e)}")
         return None, None
 
-def validar_movimiento():
-    pass
-def analizar_ritmo():
-    pass
+ # Verifica si el movimiento está dentro del umbral permitido
+
+def validar_movimiento(movimiento):
+    return movimiento <= UMBRAL_MOVIMIENTO
+ 
+# Analiza la frecuencia cardíaca y detecta anomalías
+
+def analizar_ritmo(paciente):
+    if paciente['movimiento_actual'] is None or paciente['ritmo_actual'] is None:
+        return
+   
+    if not validar_movimiento(paciente['movimiento_actual']):
+        paciente['alertas'].append("Afectado por movimiento - Lectura invalidada")
+        return
+   
+    if paciente['ritmo_actual'] < FRECUENCIA_CARDIACA_MINIMA:
+        paciente['alertas'].append(f"Bradicardia crítica: {paciente['ritmo_actual']:.1f} bpm")
+    elif paciente['ritmo_actual'] > FRECUENCIA_CARDIACA_MAXIMA:
+        paciente['alertas'].append(f"Taquicardia alarmante: {paciente['ritmo_actual']:.1f} bpm")
+
+# Inicializa el archivo de registro .log para almacenar las lecturas
 def iniciar_registro():
-    pass
+    try:
+        with open('registro_UCI.log', 'w', encoding='utf-8') as registro:
+            registro.write("Registro de Monitoreo UCI/UCE - Protocolo NCC MERP\n")
+    except Exception as e:
+        print(f"Error en simulación: {str(e)}")
 
 def registro_lectura(): 
     try:
@@ -85,4 +106,4 @@ def main():
 if __name__ == 'main': #con esto hacemos que el codigo inicie por la funcion main()
 
     main()
-    print("Hola!")
+
