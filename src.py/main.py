954c289ba2c817c9f5513747af1bfcccfd1bf617
Ramirezsara09 <alvarezz.sara@uces.edu.co>
2025-04-01 11:53:55 -0500

    PEP8 (caracteres)

--- src.py/main.py
+++ src.py/main.py
@@ -35,22 +35,33 @@ def validar_movimiento(movimiento):
 
 # Analiza la frecuencia cardíaca y detecta anomalías
 def analizar_ritmo(paciente):
-    if paciente['movimiento_actual'] is None or paciente['ritmo_actual'] is None:
+    if (
+        paciente['movimiento_actual'] is None
+        or paciente['ritmo_actual'] is None
+    ):
         return
     if not validar_movimiento(paciente['movimiento_actual']):
-        paciente['alertas'].append("Afectado por movimiento - Lectura invalidada")
+        paciente['alertas'].append(
+            "Afectado por movimiento - Lectura invalidada"
+            )
         return
     if paciente['ritmo_actual'] < FRECUENCIA_CARDIACA_MINIMA:
-        paciente['alertas'].append(f"Bradicardia crítica: {paciente['ritmo_actual']:.1f} bpm")
+        paciente['alertas'].append(
+            f"Bradicardia crítica: {paciente['ritmo_actual']:.1f} bpm"
+            )
     elif paciente['ritmo_actual'] > FRECUENCIA_CARDIACA_MAXIMA:
-        paciente['alertas'].append(f"Taquicardia alarmante: {paciente['ritmo_actual']:.1f} bpm")
+        paciente['alertas'].append(
+            f"Taquicardia alarmante: {paciente['ritmo_actual']:.1f} bpm"
+            )
 
 
 # Inicializa el archivo de registro .log para almacenar las lecturas
 def iniciar_registro():
     try:
         with open('registro_UCI.log', 'w', encoding='utf-8') as registro:
-            registro.write("Registro de Monitoreo UCI/UCE - Protocolo NCC MERP\n")
+            registro.write(
+                "Registro de Monitoreo UCI/UCE - Protocolo NCC MERP\n"
+                )
     except Exception as e:
         print(f"Error en simulación: {str(e)}")
 
@@ -58,7 +69,8 @@ def iniciar_registro():
 # Registro de datos en un archivo .log
 def registro_lectura(paciente):
     try:
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")  # se tiene en cuenta la hora y fecha para el ciclo
+        # Se tiene en cuenta la hora y fecha para el ciclo
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
 
         with open('registro_UCI.log', 'a', encoding='utf-8') as registro:
             status = "VALIDA"
@@ -67,10 +79,16 @@ def registro_lectura(paciente):
             else:
                 status = "NO VALIDA"
             registro.write(f"\n{timestamp} | Estado: {status}")
-            registro.write(f" | Movimiento: {paciente['movimiento_actual']:.2f} G")
+            registro.write(
+                f" | Movimiento: {paciente['movimiento_actual']:.2f} G"
+                )
             if status == "VALIDA":
-                registro.write(f" | Frecuencia cardiaca: {paciente['ritmo_actual']:.1f} bpm")
-                print(f"[LECTURA] Frecuencia cardíaca: {paciente['ritmo_actual']:.1f} bpm")
+                registro.write(
+                    f" | Frecuencia cardiaca: {paciente['ritmo_actual']:.1f} bpm"
+                    )
+                print(
+                    f"[LECTURA] Frecuencia cardíaca: {paciente['ritmo_actual']:.1f} bpm"
+                    )
             if paciente['alertas']:
                 registro.write(f" | ALERTAS: {', '.join(paciente['alertas'])}")
                 for alerta in paciente['alertas']:
@@ -98,6 +116,7 @@ def main():
         print('Monitoreo finalizado')
     except KeyboardInterrupt:
         print('Interrupcion de emergencia')
+
 
 # Con esto hacemos que el codigo inicie por la funcion main()
 if __name__ == '__main__':
